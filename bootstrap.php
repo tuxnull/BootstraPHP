fbe6593a0e52cb14ea2b8db033f1d5e888fe3262
amazonshitcarshow <amazonshitcarshow@setstorm.com>
2019-01-28 13:53:44 +0100

    - Added init_bphp_ext(extension) - Fixed navbar_element - Added navbar_element autodetect_active attribute - Fixed jumbotron_content - Added ext_footer(content)

--- bootstrap.php
+++ bootstrap.php
@@ -22,6 +22,12 @@ function init_bootswatch_stylesheet($style){
     return '<link rel="stylesheet" href="https://bootswatch.com/4/'.$style.'/bootstrap.min.css">';
 }
 
+function init_bphp_ext($extension){
+    if($extension == "footer"){
+        return '<link rel="stylesheet" href="https://tuxnull.com/CDN/bphpext_footer.css">';
+    }
+}
+
 function init_js(){
     return '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
@@ -77,11 +83,7 @@ function navbar_element($args){
     }
 
     if(array_key_exists("active",$args)){
-        if($args["active"] == true){
-            $active = true;
-        }else{
-            $active = false;
-        }
+        $active = $args["active"];
     }else{
         $active = false;
     }
@@ -122,17 +124,32 @@ function navbar_element($args){
         $lead_link_element = "a";
     }
 
+    if(array_key_exists("autodetect_active",$args)){
+        $autodetect_active = $args["autodetect_active"];
+    }else{
+        $autodetect_active = true;
+    }
+
+
+
+    if($autodetect_active == true){
+        if(pathinfo($_SERVER["SCRIPT_FILENAME"],PATHINFO_FILENAME).".".pathinfo($_SERVER["SCRIPT_FILENAME"],PATHINFO_EXTENSION) == $links_to){
+            $active = true;
+        }
+        if($links_to == "#"){
+            $active = true;
+        }
+    }
+
+    if($active == true){
+        $active = "active";
+    }
+
 
     if($element == "link"){
-        if($active == true){
-            return sprintf('<li class="nav-item active">
-            <a class="nav-link" href="%s">%s</a>
-            </li>',$links_to,$name);
-        }else{
-            return sprintf('<li class="nav-item">
-            <a class="nav-link" href="%s">%s</a>
-            </li>',$links_to,$name);
-        }
+        return sprintf('<li class="nav-item %s">
+        <a class="nav-link" href="%s">%s</a>
+        </li>',$active,$links_to,$name);
     }
     if($element == "dropdown"){
         return sprintf('<li class="nav-item dropdown">
@@ -518,12 +535,12 @@ function jumbotron_content($args, $content){
     if(array_key_exists("title_class", $args)){
         $title_class = $args["title_class"];
     }else{
-        $title_class = "display_4";
+        $title_class = "display-4";
     }
 
 
     if($button_text != ""){
-        $button_text = '<'.$button_type.' class="'.$button_style.'" href="'.$links_to.'" role="button">'.$button_text.'</'.$button_type.'>';
+        $button_text = '<'.$button_type.' class="'.$button_style.'" '.$links_to.' role="button">'.$button_text.'</'.$button_type.'>';
     }
 
     return '<'.$title_tag.' class="'.$title_class.'">'.$title.'</'.$title_tag.'>
@@ -885,7 +902,13 @@ function list_group($content){
     return '<ul class="list-group">'.$content.'</ul>';
 }
 
-
+function ext_footer($content){
+    return '<footer class="footer">
+      <div class="container">
+        <span class="text-muted">'.$content.'</span>
+      </div>
+    </footer>';
+}
 
 
 ?>
