91752d6f6e837cc09a44aace21da3686a3e50c0c
amazonshitcarshow <amazonshitcarshow@setstorm.com>
2019-01-29 01:22:47 +0100

    Switched default stylesheet location to bootstraPHP CDN

--- bootstrap.php
+++ bootstrap.php
@@ -15,7 +15,7 @@ function page_title($title){
 }
 
 function init_stylesheet(){
-    return '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">';
+    return '<link rel="stylesheet" href="https://tuxnull.com/CDN/bootstrap.min.css">';
 }
 
 function init_bootswatch_stylesheet($style){
