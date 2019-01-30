cd06d239e3bed91981784a019fdbda4bdb091043
amazonshitcarshow <amazonshitcarshow@setstorm.com>
2019-01-30 21:25:32 +0100

    Removed robots index, follow meta in init_meta()

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ echo "<meta name='bootstraPHP_docs' content='https://tuxnull.com'>";
 
 
 function init_meta(){
-    return '<meta charset="utf-8"><meta name="robots" content="index,follow">
+    return '<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
 }
 
